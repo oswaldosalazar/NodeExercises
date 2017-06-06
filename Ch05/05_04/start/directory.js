5db6c0f76213bb4dc1ed765621d990926c10e9d4
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-06-06 16:17:15 -0600

    mkdir

---
+++ Ch05/05_04/start/directory.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+if(fs.existsSync('lib')){
+  console.log("Directory already there");
+} else {
+  fs.mkdir('lib', (err) => {
+    if(err){
+      console.log(err);
+    } else {
+      console.log("Directory Created");
+    }
+  });
+}
