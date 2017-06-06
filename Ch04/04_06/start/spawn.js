d0176957ac6fa16e681a73ccb365d287cc3046ec
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-06-06 11:27:01 -0600

    Exec, spawn

---
+++ Ch04/04_06/start/spawn.js
@@ -0,0 +1,17 @@
+const spawn = require('child_process').spawn;
+
+let cp = spawn("node", ["alwaysTalking"]);
+
+cp.stdout.on("data", (data) => {
+  console.log(`STDOUT: ${data.toString()}`);
+});
+
+cp.on("close", () => {
+  console.log("Child Process has ended");
+
+  process.exit();
+});
+
+setTimeout( () => {
+  cp.stdin.write("stop");
+}, 4000)
